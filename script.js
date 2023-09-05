64e2e98f15d148bd7fe003eb1e8aaadb276af1f7
G P GUHAA PRIYAN <125464557+GUH1N@users.noreply.github.com>
2023-09-05 15:23:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function Addition(){
+    var a = document.getElementById("NO1").value;
+    var b = document.getElementById("NO2").value;
+    var c = a+b;
+    document.getElementById("out1").innerHTML=c
+}
+function Subraction(){
+    var a = document.getElementById("NO1").value;
+    var b = document.getElementById("NO2").value;
+    var c = a-b;
+    document.getElementById("out2").innerHTML=c
+}
+function Multipication(){
+    var a = document.getElementById("NO1").value;
+    var b = document.getElementById("NO2").value;
+    var c = a*b;
+    document.getElementById("out3").innerHTML=c
+}
+function Division(){
+    var a = document.getElementById("NO1").value;
+    var b = document.getElementById("NO2").value;
+    var c = a/b;
+    document.getElementById("out4").innerHTML=c
+}
